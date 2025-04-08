c8e979d5829f0810dc34bbb47d6b121ead5f6ddf
BlinovaD2007 <dblinova078@gmail.com>
2025-04-08 16:39:37 +0300

    Add files via upload

---
+++ task_str.py
@@ -0,0 +1,31 @@
+# Задача 1
+# volume_disket = 1.44 * 1024 * 1024
+# page = 100
+# line = 50
+# symbol = 25
+# inf_symbol = 4
+# volume_book = page * line * symbol * inf_symbol
+# num_book = volume_disket // volume_book
+# print(num_book)
+
+# Задача 2
+# pi = 3.1415
+# radius = 5
+# side = 5
+# perimetr = 4 * side
+# area_circle = pi * radius**2
+# circle = 2 * pi * radius
+# print(round(area_circle,2))
+
+# Задача 3
+# s = 20*'0' + 50*'1' + 30*'2'
+# str_numbers = s
+# print(str_numbers)
+
+# Задача 4
+# speed = 4096
+# time = 120
+# coast = speed * time
+# size = 500
+# money = coast // size
+# print(money)
